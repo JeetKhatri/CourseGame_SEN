3743ae490ed57aa38f838c597ee905048cfb4b94
Jeet Khatri <jeet.g.khatri@gmail.com>
2017-11-03 22:16:36 +0530

    Update DBConnection.java

--- src/main/java/root/Utils/DBConnection.java
+++ src/main/java/root/Utils/DBConnection.java
@@ -11,7 +11,8 @@ public class DBConnection {
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/coursegame", "root", "");
+			//connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/coursegame", "root", "");
+			connection = DriverManager.getConnection("jdbc:postgresql://ec2-107-20-193-74.compute-1.amazonaws.com:5432/dco88md9862m6k?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory","gbbqoijsobpteu", "33c58383428b29b3a9f869afaf11e6b9ebdc262777ccfa1a55bee243c0e70b8e");
 			if (connection != null) {
 				System.out.println("Connected");
 			} else {
