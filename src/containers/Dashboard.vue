5c402f38e149c2922ac3bb8086b5376b5576a34c
vamsi240022 <vamsi240022@gmail.com>
2017-11-05 02:06:51 +0530

    recreating dashboard

--- src/containers/Dashboard.vue
+++ src/containers/Dashboard.vue
@@ -1,44 +1,16 @@
 <template>
 	<div class="dashboard">
-		<navbar></navbar>
-		<div class="container">
-			<div class="columns is-multiline is-mobile">
-				<div class="column is-one-quarter">
-					<sidebar></sidebar>
-				</div>
-				<div class="column">
-					<div class="card main">
-						<!-- <router-view></router-view> -->
-						<h2>Hello</h2>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+		<h1>Hello</h1>
+	</div>	
 </template>
 
 <script type="text/javascript">
-import navbar from '@/components/Navbar';
-import sidebar from '@/components/sidebar';
 export default {
 	name: 'dashboard',
 
-	components: {
-		navbar,
-		sidebar
-	}
 }
 </script>
 
 <style lang="scss">
-.dashboard {
-	.main {
-		min-height: auto;
-		max-height: auto;
-		height: 40rem;
-		top: 0.8rem;
-	}
 
-
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import Vuetify from 'vuetify'
-
 import 'bulma/css/bulma.css'
 
 Vue.config.productionTip = false
-// Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
