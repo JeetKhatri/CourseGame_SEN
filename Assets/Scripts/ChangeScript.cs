84dc9fa7179c920fcdc145b29132f5996cedf37a
Chandni Thakkar <chandnithakkar95@gmail.com>
2017-11-16 13:57:01 +0530

    Initial Commit
    Initial Saved

---
+++ Assets/Scripts/ChangeScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//#if UNITY_ANDROID
+//using UnityEngine.WSA;
+//#endif
+
+public class ChangeScript : MonoBehaviour
+{
+	
+	public void changeScript ()
+	{
+		SceneManager.LoadScene ("MAIN");
+	}
+
+	public void nevigateTOGame ()
+	{
+		SceneManager.LoadScene ("GameMenu");
+	}
+
+	public void NavigateTOSetting ()
+	{
+		SceneManager.LoadScene ("SettingMenu");
+	}
+
+	public void NavigateTOForgot ()
+	{
+		SceneManager.LoadScene ("forgotpassword");
+	}
+
+	public void NavigateTOLogin ()
+	{
+		SceneManager.LoadScene ("login");
+	}
+	public void Update ()
+	{
+		
+		if (UnityEngine.Application.platform == RuntimePlatform.Android) {
+
+			if (Input.GetKeyUp (KeyCode.Escape)) {
+				Scene scene = SceneManager.GetActiveScene ();
+				string sceneString = scene.name;
+				if (sceneString == "SettingMenu") {
+					changeScript ();
+				} else if (sceneString == "GameMenu") {
+					changeScript ();
+				} else if (sceneString == "Main") {
+					UnityEngine.Application.Quit ();
+				} else if (sceneString == "forgotpassword") {
+					NavigateTOLogin ();
+				}
+			} else {
+				UnityEngine.Application.Quit ();	
+			}
+		}
+
+	}
+		
+	//public void MakeToast(){
+	//	Toast t=null;
+	//	t =Toast.Create ("","Hello World");
+//		t.Show ();
+//	}
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# CourseGame_SEN
-
