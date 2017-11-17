b6ccbf93d099f20268112c85fdcef8ac8dde02e3
Jeet Khatri <jeet.g.khatri@gmail.com>
2017-11-17 15:36:23 +0530

    Student Change
    Student Change

--- src/main/java/root/WebServices/StudentWebService.java
+++ src/main/java/root/WebServices/StudentWebService.java
@@ -3,14 +3,7 @@ package root.WebServices;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.*;
 
 import root.Bean.StatusBean;
 import root.Bean.StudentBean;
